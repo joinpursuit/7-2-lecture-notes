871c4ea89c7926eb9524640a905dcb06b9ae438c
Corey Ladovsky <coreyskymusic@gmail.com>
2021-01-09 14:26:12 -0800

    array methods

--- MODULE1/arrayMethods/arrayMethods.js
+++ MODULE1/arrayMethods/arrayMethods.js
@@ -28,7 +28,7 @@ const printElement = (el) => {
 // console.log(typeof printElement)
 
 const printElementsForEach = (arr) => {
-  returnarr.forEach((el, i) => {
+  arr.forEach((el, i) => {
     console.log(`Index is ${i} and elment is ${el}`);
   });
 };
@@ -43,7 +43,7 @@ const printElementsForEach = (arr) => {
 // sum: 10
 // el: 4
 
-//forEach is use for it's side effects 
+//forEach is use for it's side effects
 
 const sumElements = (arr) => {
   let sum = 0;
@@ -57,14 +57,14 @@ const sumElements = (arr) => {
 // console.log(sumElements([1, 2, 3, 4]));
 
 const myIncludes = (arr, target) => {
-    arr.forEach((el) => {
-        if(el === target) {
-            return true;  // does not work. CANNOT return from forEach callback. 
-        }
-    })
+  arr.forEach((el) => {
+    if (el === target) {
+      return true; // does not work. CANNOT return from forEach callback.
+    }
+  });
 
-    return false; 
-}
+  return false;
+};
 
 // console.log(myIncludes(["cat", "dog", "bird"], "pig"))
 // console.log(myIncludes(["cat", "dog", "bird"], "dog"))
@@ -76,25 +76,182 @@ const myForEach = (arr, callbackFunction) => {
 };
 
 const findProduct = (numbers) => {
-  let prod = 1; 
+  let prod = 1;
   numbers.forEach((num) => {
-    prod *= num
-  })
+    prod *= num;
+  });
 
-  return prod; 
-}
+  return prod;
+};
 
 // console.log(findProduct([1, 2, 3, 4, 5]))
 
-// write a function that takes in an array and returns a 
-// new array with every element in the array doubled. 
+// write a function that takes in an array and returns a
+// new array with every element in the array doubled.
+
+// const doubleArr = (arr) => {
+//   let output = [];
+//   arr.forEach((el, i) => {
+//     output.push(el * 2);
+//   })
+//   return output;
+// }
+
+//map is another array method -> evaluates to a new array.
+// it iterates through an array and calls the callback for every element and pushes the result into the new array.
+
+const doubleEl = (el) => {
+  return el * 2;
+};
 
 const doubleArr = (arr) => {
+  return arr.map((el) => {
+    return el * 2;
+  });
+};
+
+// console.log(doubleArr([1, 2, 3])); // => [2, 4, 6]
+
+const myMap = (arr, callback) => {
   let output = [];
-  arr.forEach((el, i) => {
-    output.push(el * 2);
+  for (let i = 0; i < arr.length; i++) {
+    output.push(callback(arr[i], i));
+  }
+  return output;
+};
+
+//write a function that iteraters through and array and returns each element + 2. 
+
+const plusTwo = (arr) => {
+  let output = arr.map((el) => {
+    return el + 2; 
   })
   return output; 
 }
 
-console.log(doubleArr([1, 2, 3])) // => [2, 4, 6]
+// console.log(plusTwo([1, 2, 3, 4]))
+
+// Write a function that takes in an array of strings and returns 
+// a new arrray where every stirng has been capitilaized and has an ! at the end. 
+
+const shout = (words) => {
+  return words.map((word) => {
+      return word.toUpperCase() + "!";
+  })
+}
+
+// console.log(shout(["hello", "world"])) // => ["HELLO!", "WORLD!"]
+
+//write a function that takes in array and returns a new array where every element 
+// has been mulitipled by its index
+
+// const multByIndex = (numbers) => {
+//   return numbers.map((num, i) => {
+//     return num * i;
+//   })
+// }
+
+// console.log(multByIndex([1, 2, 3, 4, 5])) // => [0, 2, 6, 12, 20]
+
+// const sum = (arr) => {
+//   let sum = 0; 
+//   arr.map(el => { // DONT USE MAP FOR SIDE EFFECTS LIKE THIS! 
+//     sum += el
+//   })
+//   return sum; 
+// }
+// console.log(sum([5, 5, 6]))
+
+//filter is an array method that evaluates to a new array (non mutative) of only the elements 
+// that had a truthy return from the callback. 
+
+const onlyStrings = (arr) => {
+  let output = arr.filter((el) => {
+    return typeof el === "string"
+  })
+
+  return output; 
+}
+// console.log(onlyStrings(["hello", 4, 5, "cat"]));
+
+//write a function that takes in an array and filters it to only have odd numbers.
+
+const onlyOdds = (nums) => {
+  return nums.filter((el) => {
+    return el % 2 === 1; 
+  })
+}
+
+// console.log(onlyOdds([1, 2, 3, 4, 5])) // => [1, 3, 5]
+
+//write a function that takes in an array 
+// and returns a new array with only words that were already capitalized.
+
+const onlyCaps = words => {
+  return words.filter((word) => {
+    return word === word.toUpperCase();
+  })
+}
+
+// console.log(onlyCaps(["Hello", "CAT", "word"])) // => ["CAT"]
+
+const myFilter = (arr, callback) => {
+  let output = [];
+  for(let i = 0; i < arr.length; i++) {
+    if(callback(arr[i])) {
+      output.push(el)
+    } 
+  }
+  return output; 
+}
+
+// every is an array method that evaluates into a boolean. 
+// It checks to see if every element passed into the callback evaluates to truthy. 
+
+const isAllNumsFive = (nums) => {
+  return nums.every((num) => {
+    return num === 5; 
+  })
+}
+
+// console.log(isAllNumsFive([5, 6, 5]))
+
+// //write a function that takes in an array and determines if every element in the array 
+// // is even
+
+const allEvens = (nums) => {
+  return nums.every((num) => {
+    return num % 2 === 0; 
+  })
+}
+// console.log(allEvens([2, 4, 6])) // => true
+// console.log(allEvens([2, 4, 7])) // => false
+
+//write a function that determines if all words in an array of a length of five or more. 
+
+const allFiveOrMore = (words) => {
+  return words.every((word) => {
+    return word.length >= 5
+  })
+}
+
+// console.log(allFiveOrMore(["hello", "dollys"])) // => true 
+// console.log(allFiveOrMore(["hello", "cat"])) // => false 
+
+const myEvery = (arr, callback) => {
+  for(let i = 0; i < arr.length; i++) {
+    if(!callback(arr[i], i)) {
+      return false;
+    }
+  }
+  return true; 
+}
+
+// Sort! Sort is an array method, that sorts our data. => is mutative 
+// let arr = ["sparky", "mike", "corey", "sam", "peter"];
+// arr.sort();
+// console.log(arr)
+
+// let nums = [2, 1, 11, 12, 5]
+// nums.sort((a, b) => a - b);
+// console.log(nums)
