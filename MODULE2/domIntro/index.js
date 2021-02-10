480682f02a6800aab47531101e109d63e2507f69
Corey Ladovsky <coreyskymusic@gmail.com>
2021-02-09 18:42:21 -0800

    replaceWithStar

--- MODULE2/domIntro/index.js
+++ MODULE2/domIntro/index.js
@@ -40,3 +40,20 @@ const changeHeader = () => {
   const h1 = document.getElementById("h1-tag");
   h1.innerText = input.value;
 }
+
+const replaceWithStar = () => {
+    const input = document.getElementById("replace-with-star");
+    const p = document.getElementById("text-with-star");
+    const str = input.value;
+    let output = "";
+    const vowels = "aeiouAEIOU";
+    for(const char of str) {
+        if(vowels.includes(char)) {
+            output += "*";
+        } else {
+            output += char; 
+        }
+    }
+
+    p.innerText = output; 
+}
