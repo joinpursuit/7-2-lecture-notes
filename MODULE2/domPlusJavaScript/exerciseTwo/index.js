413bbcb769c8dea1b3d778ef5924ad82040809bf
Sparky Evangelista <sparkyevangelista@pursuits-mbp.myfiosgateway.com>
2021-02-13 10:32:10 -0500

    fixed more typos

--- MODULE2/domPlusJavaScript/exerciseTwo/index.js
+++ MODULE2/domPlusJavaScript/exerciseTwo/index.js
@@ -1,6 +1,6 @@
 // Solve this solution using querySelectorAll and querySelector 
 // <!-- 
-// Create a feature that grabs all of these element
+// Create a feature that grabs all of the li elements
 // and sums up all the numbers, when a user clicks on a button.
 // The total should appear as the span's text
 // -->
