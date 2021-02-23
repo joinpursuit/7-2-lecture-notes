d9446619d79e76576fdc38f4e123d6c3860aa29b
Sparky Evangelista <sparkyevangelista@pursuits-mbp.myfiosgateway.com>
2021-02-23 14:44:29 -0500

    added flexbox intro folder and some images

--- MODULE2/domPlusJavaScript/exerciseFive/index.js
+++ MODULE2/domPlusJavaScript/exerciseFive/index.js
@@ -3,8 +3,18 @@
 // multiplied by two using the replaceChild method
 
 function changeOdds(){
-    //querySelector to grab all p tags
+    //querySelectorAll to grab all p tags
+    const ps = document.querySelectorAll(".number");
     //loop through the p tags and find odd numbers (%)
+    for(let p of ps){
+        console.log(p)
+        let number = Number(p.textContent);
+        if(number % 2 === 1){
+            let newP = document.createElement("p");
+            newP.textContent = number * 2;
+            p.parentNode.replaceChild(newP,p);
+        }
+    }
     //if true, get the textContent mutiple the number by 2
     //create a new element and give it the number 
     //using the replaceChild change the old Element with the new Element
