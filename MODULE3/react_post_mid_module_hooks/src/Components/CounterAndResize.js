10a5f2dc86aa4e5acf820849326ce07b3469ea36
Corey Ladovsky <coreyskymusic@gmail.com>
2021-05-08 08:39:15 -0700

    update with hooks

--- MODULE3/react_post_mid_module_hooks/src/Components/CounterAndResize.js
+++ MODULE3/react_post_mid_module_hooks/src/Components/CounterAndResize.js
@@ -1,27 +1,55 @@
+import React, { useState, useEffect } from "react";
 
-import React, { useState } from 'react';
+// useState is a React hook that takes initialState as the argument and returns
+// a two element array. The first element is the state. The second element is a
+// function to set the state.
 
-// useState is a React hook that takes initialState as the argument and returns 
-// a two element array. The first element is the state. The second element is a 
-// function to set the state. 
+// useEffect - replaces componentDidMount, componentDidUpdate, componentWillUnmount
+// takes in two arguments
+//    1st - callback to run (on mount on update)
+//    2nd - dependency array - any changes to the dependency will cause hook to run
+// empty array makes it componentDidMount only
+
+// if it returns a function, that function will be called on unmount
+
+// cannot nest hooks or conditionally use them
+// can make custom hooks - another day
 
 const CounterAndResize = () => {
-    const [count, setCount] = useState(0)
-    const [ windowSize, setWindowSize ] = useState(window.innerWidth);
+  const [count, setCount] = useState(0);
+  const [windowSize, setWindowSize] = useState(window.innerWidth);
 
-    return (
-      <section>
-        <div>
-          <h2>Window Size: {windowSize}</h2>
-        </div>
-        <p>{count}</p>
-        <button onClick={() => setCount((prevCount) => prevCount + 1)}>
-          +
-        </button>
-      </section>
-    );
-    
-}
+  useEffect(() => {
+    const handleResize = () => {
+      console.log("WINDOW RESIZE");
+      setWindowSize(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    console.log("HOOK RAN");
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      console.log("COMPONENT WILL UNMOUNT");
+    };
+  }, []);
+
+//   useEffect(() => {
+//     const handleResize = () => setWindowSize(window.innerWidth);
+//     window.addEventListener("resize", handleResize);
+//     return () => window.removeEventListener("resize", handleResize);
+//   }, []);
+
+  return (
+    <section>
+      <div>
+        <h2>Window Size: {windowSize}</h2>
+      </div>
+      <p>{count}</p>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>+</button>
+      <button onClick={() => setCount((prevCount) => prevCount - 1)}>-</button>
+      <button onClick={() => setCount(0)}>Reset To Zero</button>
+    </section>
+  );
+};
 // const CounterAndResize = () => {
 //     const [count, setCount] = useState(0)
 //     const [ windowSize, setWindowSize ] = useState(window.innerWidth);
@@ -36,10 +64,10 @@ const CounterAndResize = () => {
 //     //     setCount((prevCount) => prevCount + 1)
 //     // }
 
-//     // handleResize = () => {
-//     //     console.log("WINDOW RESIZE")
-//     //     this.setState({windowSize: window.innerWidth});
-//     // }
+// handleResize = () => {
+//     console.log("WINDOW RESIZE")
+//     this.setState({windowSize: window.innerWidth});
+// }
 
 //     // componentDidMount() {
 //     //     window.addEventListener("resize", this.handleResize)
@@ -49,7 +77,7 @@ const CounterAndResize = () => {
 //     //     window.removeEventListener("resize", this.handleResize)
 //     // }
 
-//     // const { count, windowSize } = state; 
+//     // const { count, windowSize } = state;
 //     return (
 //       <section>
 //         <div>
@@ -61,7 +89,7 @@ const CounterAndResize = () => {
 //         </button>
 //       </section>
 //     );
-    
+
 // }
 // class CounterAndResize extends Component {
 //     state = { count: 0, windowSize: window.innerWidth}
@@ -85,7 +113,7 @@ const CounterAndResize = () => {
 //     }
 
 //     render() {
-//         const { count, windowSize } = this.state; 
+//         const { count, windowSize } = this.state;
 //         return(
 //             <section>
 //                 <div>
@@ -98,4 +126,4 @@ const CounterAndResize = () => {
 //     }
 // }
 
-export default CounterAndResize
+export default CounterAndResize;
