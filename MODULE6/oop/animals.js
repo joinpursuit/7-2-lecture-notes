e78524ff48d78513f6dacc2626a429e4f95e2211
Gigi Scarborough <gigimscarborough@gmail.com>
2021-10-17 11:26:19 -0400

    OOP notes

--- MODULE6/oop/animals.js
+++ MODULE6/oop/animals.js
@@ -6,12 +6,33 @@ let buttons = {
     greeting: "sniff sniff",
     walk: "clip clop, clip clop",
 }
+
 // WHY IS THE ABOVE NOT IDEAL?
 
 // OBJECT-ORIENTED IMPLEMENTATION OF A ANIMAL CLASS
+
 class Animal {
+    constructor(name, type, age, color, isFriendly=false ){
+        this.name = name
+        this._type = type;
+        this.age = typeof age != "number" ? 1 : age;
+        this.color = (color || "black");
+        this.isFriendly = isFriendly;
+    }
 
     //ADD METHODS TO ANIMAL CLASS
+    walk(){
+        console.log("clip, clop")
+    }
+
+    greet(otherBeing) {
+        console.log(`Sniff sniff, ${otherBeing.name}, I like your ${otherBeing.color} fur.`);
+    }
+
+    ageUp(){
+        this.age++
+    }
+    
 
     // ADD MULTIPLE METHODS TO OUR CLASS AND PASS THEM ARGUEMENTS
 
@@ -27,6 +48,43 @@ class Animal {
 //WHY IS THIS BETTER?
 
 // CONSOLE.LOG NEW INSTANCES OF OUR CLASS
+
+// const fluffy = new Animal("Fluffy", "miniature horse", 4);
+// console.log(fluffy)
+// console.log(fluffy.color)
+// console.log(fluffy.type)
+// fluffy.walk()
+// fluffy.greet("Fido")
+// console.log(fluffy.age) // before age up
+// fluffy.ageUp()
+// fluffy.ageUp()
+// fluffy.ageUp()
+// console.log(fluffy.age) // after age up
+// const fido = new Animal("Fido", "dog", 12, "white", false)
+// console.log(fido)
+
+// fluffy.greet(fido)
+// fido.greet(fluffy)
+
+
+
+class StringExtra {
+    static reverse(str) {
+      return str.split("").reverse().join("");
+    }
+    static description =
+      "I have some extra methods for doing things with strings";
+  }
+  
+  
+  console.log(StringExtra.reverse("I palindrome I"));
+
+  console.log(StringExtra.description);
+ 
+
+
+// console.log(fido.age)
+
 
 // INVOKE INSTANCE METHODS ON OUR NEW INSTANCES OF THE ANIMAL CLASS
 
