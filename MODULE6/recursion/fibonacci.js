a87c842167569b9c566ea77d8fbef2e73dc0a246
tim mckiernan <tim@pursuit.org>
2021-10-18 19:04:24 -0400

    before pulling after change

--- MODULE6/recursion/fibonacci.js
+++ MODULE6/recursion/fibonacci.js
@@ -9,10 +9,10 @@
 
 const happyNewYear = (num) => {
   if (num === 0){
-    console.log("HAPPY NEW YEAR")
+    console.log("HAPPY NEW YEAR");
   } else {
-    console.log(`${num}...`)
-    happyNewYear(num - 1)
+    console.log(`${num}...`);
+    happyNewYear(num - 1);
   }
 };
 
@@ -25,30 +25,24 @@ const happyNewYear = (num) => {
 //  this function in only for displaying recursion - dont use it :)
 const isEven = (num) => {
   if ( num === 0 ){
-    return "number is even"
+    return "number is even";
   }
   if( num === 1 ){
-    return "number is odd"
+    return "number is odd";
   }
-  return isEven(num - 2)
+  return isEven(num - 2);
 }
-const nums = [ 1,2,3,5,6 ]
+const nums = [ 1,2,3,5,6 ];
 // expected outcome is 17
 //  add all items in an array and return the sum
 //  default parameter only happens when the argument is undefined
 const addAll = (arr, sum = 0) => {
-
   if (arr.length === 0){
-    return sum
+    return sum;
   }
-  sum += arr.pop()
+  sum += arr.pop();
   return addAll(arr, sum);
 }
-
-
-
-
-
 
 // write a fucntion that prints the nth number 
 // in the FIBONACCI SEQUENCE!@#!@#
@@ -57,20 +51,21 @@ const addAll = (arr, sum = 0) => {
 //  fib(1) => 1
 const fibo = (num) => {
   if (num <= 1){
-    return 1
+    return 1;
   }
-  return fibo(num - 1) + fibo(num - 2)
+  return fibo(num - 1) + fibo(num - 2);
  }
 
 const fibMemo = (num, memo = []) =>{
   if( num <=1 ){
-    return 1
+    return 1;
   } else if (!memo[num]){
-    memo[num] = fibMemo(num - 1, memo) 
-    + fibMemo(num - 2, memo)
+    memo[num] = fibMemo(num - 1, memo)  + fibMemo(num - 2, memo);
    }
-   return memo[num]
+   return memo[num];
 }
-console.time("fibmemo")
-console.log(fibo(30))
-console.timeEnd("fibmemo")
+
+// console.time will show us how long our function executes!
+console.time("fibmemo");
+console.log(fibo(30));
+console.timeEnd("fibmemo");
