d3f9195cb7d9c6eb8c5ffaa90958af286cc6bf4e
Gigi Scarborough <gigimscarborough@gmail.com>
2021-10-24 16:49:08 -0400

    fun with mokeypatching

--- MODULE6/arrays/arrays2.js
+++ MODULE6/arrays/arrays2.js
@@ -5,8 +5,10 @@
 
 class Animal {
     static createDog() {
+        // console.log('Create Dog', this)
         return new Animal('dog');
     }
+    // static methods are for when we dont need a reference to `this`
 
     constructor(species) {
         this.species = species;
@@ -19,10 +21,16 @@ class Animal {
 
     eat(food) {
         this.stomach.push(food);
+        debugger
+    }
+
+    static createCat(){
+        return new Animal('cat')
     }
 } 
 
 window.dog = Animal.createDog();
+window.cat = Animal.createCat();
 window.fish = new Animal('fish');
 
 // find dog and fish swimming in the dev tools
@@ -37,6 +45,10 @@ window.fish = new Animal('fish');
 // monkeypatching the Array class 
 
 
+Animal.prototype.MYVARIABLE = "WHATEVER I WANT"
+
+// console.log(fish)
+
 ////////////////////////////////////////
 //////////    PRACTICE    //////////////
 ////////////////////////////////////////
@@ -46,3 +58,56 @@ window.fish = new Animal('fish');
 // [].myMap(el => el + 1);
 // [].myFilter(el => !!el);
 // [].myEvery(el => el > 0);
+
+Array.prototype.myForEach = function (callbackFn) {
+
+    for(let i = 0; i < this.length; i++){
+        callbackFn(this[i])
+    }
+}
+// in fat arrow functions `this` is the scope above the function not within
+// forEach calls a function on each element of your array and returns undefined
+
+let arr1 = [1, 2, 3]
+let logger = el => console.log(el)
+arr1.myForEach(logger)
+
+Array.prototype.myMap = function(callbackFn){
+    let array = [];
+
+    for(let i = 0; i < this.length; i++){
+        array.push(callbackFn(this[i]))
+    }
+
+    return array
+}
+
+let mapResult = arr1.myMap(el => el+1)
+// console.log(mapResult)
+
+Array.prototype.myFilter = function(callbackFn){
+    let filteredArr = [];
+
+    for(let el of this){
+        console.log(el)
+        if(callbackFn(el)){
+            filteredArr.push(el)
+        }
+    }
+    return filteredArr;
+}
+
+let filteredResult = arr1.myFilter(el => el % 2 === 0);
+// console.log(filteredResult);
+
+Array.prototype.myEvery = function(callbackFn){
+
+    for(let el of this){
+        if (!callbackFn(el)) return false
+    }
+
+    return true
+}
+
+let everyResult = arr1.myEvery(el => el % 2 === 0);
+console.log(everyResult)
