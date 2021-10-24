e35871d75004cbe245341dd489d8760d46c58a46
Gigi Scarborough <gigimscarborough@gmail.com>
2021-10-24 14:16:59 -0400

    array notes

--- MODULE6/arrays/arrays.js
+++ MODULE6/arrays/arrays.js
@@ -7,15 +7,44 @@
 */
 
 // Instantiating - how do we create a new INSTANCE of an array?
-
+    let arr1 = [];
+    // let arr2 = "string".toArray();
+    let arr3 = new Array("arg")
+    // ^^ when arg is a number and the only arg create array of that length
+    console.log(arr3)
 
 // Indexing  -  How to get elements at a certain array INDEX or change them.
-
+    let tempArr = [1, 2, 3];
+    let firstEl = tempArr[0];
+    firstEl = "something else";
+    // ^^ this will not reassign tempArr[0]
+    tempArr[0] = "something other than else";
+    // ^^ only this will reassign the value at this index in the array
+    console.log(tempArr[0]);
+    console.log(firstEl);
 
 // Higher order array functions - map, forEach, and reduce.
+    let dummyArr = [4, 5, 6];
+    const newArr = dummyArr.forEach((num) => num +1);
+    console.log(newArr);
+
+    let callbackFunc = (element) => {console.log(element)}
+    const result = dummyArr.map((element) => {console.log(element)})
+    // ^^ iterates dummyArr and calls callback fn with each element of dummyArr
+    console.log(result)
 
 // Other useful methods: sort, find, indexof, findIndex, filter, some;
 
+    function findIdx(array, el){
+        for(let i = 0; i < array.length; i++){
+            if (array[i] === el){
+                return i
+            }
+        }
+        return -1;
+    }
+
+    console.log(findIdx([0, 1, 1, 3], 1))
 /*
     It's important to remember that Array all array methods can be written
     from scratch.  We don't need to invoke Array.prototype.sum() in order to
