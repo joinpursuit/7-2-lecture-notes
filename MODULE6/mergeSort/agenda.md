ad7ddd9fd6011736711da05cb04bcaee1bd4eb18
tim mckiernan <tim@pursuit.org>
2021-11-13 15:43:13 -0500

    merge sort notes

--- MODULE6/mergeSort/agenda.md
+++ MODULE6/mergeSort/agenda.md
@@ -2,9 +2,10 @@
 	## Merge Sort
 		1. What is merge sort?
 	    2. Why whould we use it?
-		3. Build it out!
-			- Divide 
+		3. Divide and Conquer 
+		4. Build it out!
 			- Conquer
+			- Divide 
 	## Array.prototype.sort()
 		1. .sort() Under the Hood
 		2. Working with Objects
--- MODULE6/mergeSort/merge-sort.js
+++ MODULE6/mergeSort/merge-sort.js
@@ -2,7 +2,7 @@
     
      ********  MERGE SORT  **********
 
-    Merge sort is one of the most efficient sorting algorithms. 
+    Merge sort is an efficient sorting algorithm. 
     It works on the principle of Divide and Conquer. 
     Merge sort repeatedly breaks down a list into several sublists 
     until each sublist consists of a single element and merging 
@@ -17,17 +17,27 @@
     Merge sort is a fast, stable sort,
     meaning it will keep the elements in original order 
     in the case of duplicate or equal values.
-    Time complexity: O(n(logn));
+    Time complexity: O(n(logn)) N time to reassemble and log(n) to split;
     Space is O(n) - one of its weaknesses.
     Merge sort is great in multi-threaded languages like Java
-    as threads working side by side can sort the arrays while they are bing
+    as threads working side by side can sort the arrays while they are being
     split at once.
 
+    DIVIDE AND CONQUER
 
-    
-    Merge sort by the numbers 
+      In CS, Divide and Conquer is a term used to describe a certain 
+      paradigm for implementing algorithms.  Any algorithm that adheres
+      to a Divide and Conquer methodology functions by breaking a problem
+      down into a smaller version of the same problem.  This effect is 
+      repeated until a simple solution can be found.  The results are then 
+      recompiled to return a solution.
+
+
+
+Merge sort by the numbers 
 
     Imagine we need to sort the following array:
+
     [4, 8, 7, 2, 11, 1, 3]
 
     1.  DIVIDE - Split the array in two
@@ -37,10 +47,10 @@
     2.  Continue splitting until all arrays are SINGLE ELEMENTS
 
          [4, 8, 7, 2], [11, 1, 3]
-              |             |
+            /   \          /  \
         [4, 8]  [7, 2] [11,1] [3]
-          |        |      |     
-        [4], [8], [7], [2], [11], [1], [3]
+          /\      /\        /\   \  
+        [4] [8] [7] [2], [11] [1] [3]
         
     3. CONQUER - pushing the elements into a new array one by one
 
@@ -49,13 +59,129 @@
           [4, 8], [2, 7],  [1, 11] , [3]
 
     4.  Continue until we have one array
-        [ 4, 8], [2, 7],  [1, 11] , [3]
-            |       |      |     |
+        [ 4, 8], [2, 7],  [1, 11], [3]
+            \      |        |      /
           [2, 4, 7, 8], [1, 3, 11]
                 |            |
             [1, 2, 3, 4, 7, 8, 11]
 
-    */          
-  //  Step one LETS CONQUER
+    */
+const testArr = [4, 8, 7, 2, 11, 1, 3];
+const badSort = (arr) => {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    count += 1;
+    for (let j = 1; j < arr.length; j++) {
+      count += 1;
+    if (arr[j - 1] > arr[j]) {
+        // this is called Array destructuring assignment
+        [arr[j - 1], arr[j]] = [arr[j], arr[j - 1]];
+      }
+    }
+  }
+  console.log(count);
+  return arr;
+};
+// console.log(badSort(testArr))
+//  Step one, lets build our CONQUER step for use later
+//  once we have a DIVIDE step
+const left = [4, 8, 1];
+const right = [6, 3];
 
-let testArr = [4, 8, 7, 2, 11, 1, 3];
+const merge = (left, right) => {
+  // where to begin?
+  let sorted = [];
+  while (left.length &&  right.length) {
+    if (left[0] < right[0]) {
+      sorted.push(left.shift());
+    } else {
+      sorted.push(right.shift());
+    }
+  }
+  return [...sorted, ...left, ...right];
+};
+
+// Step TWO DIVIDE
+// now that we have a conquer function that merges
+//
+
+const mergeSort = (arr) => {
+  //  now what?
+  // BASE CASE 
+  if(arr.length <= 1){
+    return arr;
+  }
+  // REDUCTIVE CASE 
+  const mid = Math.ceil(arr.length/2);
+  const left = arr.splice(0, mid);
+  const right = arr;
+  return merge(mergeSort(left), mergeSort(right));
+};
+
+// console.log(mergeSort(testArr));
+
+
+
+
+//  ******** Array.prototype.sort() ********
+
+/* lets sort these cards using .sort()
+  from the MDN docs:
+  The sort() method sorts the elements of an array in place 
+  and returns the sorted array.  
+  The default sort order is ascending, 
+  built upon converting the elements into strings, 
+  then comparing their sequences of UTF-16 code units values
+
+  BUT we can pass a compareFunction as a callback.
+  This compare function accepts two arguments and
+   MUST return a number;
+  IF the number is > 0 - the second argument first;
+  IF the number is < 0 the first argument is sorted first;
+  returning  0 keeps the original order;
+*/
+// cards are cool.
+const cards = [
+  { face: 8, value: 8, suit: "Hearts" },
+  { face: "Queen", value: 10, suit: "Clubs" },
+  { face: 5, value: 5, suit: "Spades" },
+  { face: "Jack", value: 10, suit: "Spades" },
+  { face: 9, value: 9, suit: "Clubs" },
+  { face: "King", value: 10, suit: "Diamonds" },
+  { face: 2, value: 2, suit: "Diamonds" },
+  { face: "Queen", value: 10, suit: "Diamonds" },
+  { face: 5, value: 5, suit: "Clubs" },
+  { face: "Ace", value: 11, suit: "Diamonds" },
+  { face: "Jack", value: 10, suit: "Diamonds" },
+];
+
+//  things to REMEMBER
+//  we always have two arguments (a, b);
+//  return > 0 SWAP (b for a)
+//  if return < 0 (a, before b);
+//  if  0 remain the same.
+const cardSort = (arr) => {
+  return arr.sort((a,b) => {
+    if(a.suit < b.suit) return -1;
+    if(a.suit > b.suit) return 1;
+    if(a.value < b.value) return -1;
+    if(a.value > b.value) return 1;
+    //  we only get here if values are both 10
+    // if(a.face ==="Queen" && b.face ==='Jack') return 1;
+    // if(a.face ==="King" && b.face ==="Queen") return 1;
+    // if(a.face ==="King" && b.face ==="Jack") return 1;
+    if((a.face === "Jack" && b.face ==="Queen") || (a.face ==="Jack" && b.face ==="King") ) return -1;
+    if(a.face ==="Queen" && b.face === "King") return -1
+   
+
+
+
+    return 0
+  })
+}
+
+console.log(cardSort(cards));
+
+
+
+let stop = ""
