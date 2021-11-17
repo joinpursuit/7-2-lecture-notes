89f28dbbbebf1d7894bdbcbfb22700defc50ef29
tim mckiernan <tim@pursuit.org>
2021-11-16 21:42:30 -0500

    added linked list

--- MODULE6/hash-tables/hash-table.js
+++ MODULE6/hash-tables/hash-table.js
@@ -35,37 +35,80 @@
     A Hash table does have a worst case of O(n) time but this happens so rarely performance is usually
     closer to constant time;
     Let's implement a simple Hash table in JS
+    
+    Side note:  The four principles of OOP
 
+     ABSTRACTION - The concept of ONLY giving the user what they need
+        A car has a gas and a break - we don't need to know everything else!
+     POLYMORPHISM - the idea that a parent class can adapt to many different children
+        Dog class can have a bark method that might change for Chihuahua class VS Rottweiler class;
+     ENCAPSULATION - holding all the data of a single unit to keep it private 
+        only let it be updated via methods(hard to do in JS - no private yet). 
+     INHERITANCE - classes can extend the functionality of basic parent classes 
+        ( whateverComponent extends React.component)
 */
-
+class Node {
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+}
 
 class Hash {
     constructor() {
        this.table = new Array(127) 
        }
+
     _hash(key){
         let hash = 0;
         for (let char of key) {
             hash += key.charCodeAt(char);
         };
         return hash % this.table.length 
-    }
+    };
     set(key, value) {
        const index = this._hash(key);
-       this.table[index] = [key, value];
-    }
+    //    this.table[index] = [key, value];
+        let newNode = new Node([key, value]);
+        if (!this.table[index]){
+            this.table[index] = newNode;
+        } else {
+          newNode.next = this.table[index];
+          this.table[index] = newNode;
+        }
+    };
     get(key) {
         const index = this._hash(key);
-        return this.table[index];
-    }
-    // ABSTRACTION, 
-    // POLYMORPHISM,
-    //  ENCAPSULATION, 
-    // INHERITANCE
+        let node = this.table[index];
+        while (node) {
+            if(node.data[0] === key){
+                return node.data
+            } else {
+                node = node.next
+            }
+        }
+        return "not found"
+        // return this.table[index];
+    };
     remove(key) {
         const index = this._hash(key);
-        delete this.table[index];
-        // this.table[index] = [];
+        let node = this.table[index];
+        let counter = 0;
+        if(node.data[0] === key){
+            this.table[index] = node.next;
+            return;
+        }
+        while (node && node.data[0] !== key) {
+            counter++;
+            node = node.next;
+        }
+        let targetNode = node;
+        node = this.table[index];
+        for (let i = 1; i< counter; i ++) {
+            node = node.next;
+        }
+        node.next = targetNode.next;
+      
     };
     
 }
@@ -73,10 +116,15 @@ class Hash {
 
 const myHash = new Hash();
 
-// myHash.set("Durdona", "Excellent student")
+myHash.set("Durdona", "Excellent student");
+// console.log(myHash.get("Durdona"));
 // myHash.remove("Durdona")
 // console.log(myHash.get("Durdona"))
-console.log(myHash._hash("zugzwang"));
-console.log(myHash._hash("watch"));
+myHash.set("ap", "this will disappear");
+myHash.set("ad", "this will should be here");
+myHash.set("ae", "this will remain")
+
+myHash.remove("ae")
+console.log(myHash)
 
 
